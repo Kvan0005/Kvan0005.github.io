258c988c928ed1b3e9dce3174c38c8abc7aac690
LinSFa <kevin.vandervaeren@ulb.be>
2024-10-12 16:43:20 +0200

    update for testing

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// create a simple class with a 
+
+function redirect() {
+    var dropdown = document.getElementById('dropdown');
+    var selected = dropdown.options[dropdown.selectedIndex].text;
+    if (selected == "Homework 1") {
+        window.location.href = "./src/homework1/index.html";
+    }
+
+    if (selected == "Homework 2") {
+        window.location.href = "./src/homework2/index.html";
+    }
+}
